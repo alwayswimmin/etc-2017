993c09f457d0249155816136a7ae91162e747dd7
Ubuntu <ubuntu@bot-same.harvard.js-etc>
2017-02-26 15:46:42 +0000

    got Bot working

--- Bot.java
+++ Bot.java
@@ -10,11 +10,11 @@ public class Bot
     {
         try
         {
-            Socket skt = new Socket("test-exch-teamname", 20000);
+            Socket skt = new Socket("test-exch-same", 20000);
             BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
             PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
 
-            to_exchange.println("HELLO TEAMNAME");
+            to_exchange.println("HELLO SAME");
             String reply = from_exchange.readLine().trim();
             System.err.printf("The exchange replied: %s\n", reply);
         }
