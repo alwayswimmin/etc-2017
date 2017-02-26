033ccb14115c71f592ca0659a59f67aff34e5cd3
Ubuntu <ubuntu@bot-same.harvard.js-etc>
2017-02-26 17:53:50 +0000

    fix allison

--- Bot.java
+++ Bot.java
@@ -50,7 +50,7 @@ public class Bot
 // 				levels[i] = reply.substring(reply.indexOf(intToName(i))+reply.indexOf(intToName(i)).length(), reply.indexOf(" ", reply.indexOf(intToName(i))));
 				levels[i] = Integer.parseInt(reply.substring(reply.indexOf(":", reply.indexOf(intToName(i)))+1, reply.indexOf(" ", reply.indexOf(intToName(i)))));
 			}
-			levels[6] =  Integer.parseInt(reply.substring(reply.indexOf(":", reply.indexOf(intToName(i)))+1);
+			levels[6] =  Integer.parseInt(reply.substring(reply.indexOf(":", reply.indexOf(intToName(6)))+1));
 			
 
 			identifier = 1;
