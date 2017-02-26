6e9166ab929690864e46c921b70e828b742b8fa2
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2017-02-26 11:12:36 -0500

    hi

--- Bot.java
+++ Bot.java
@@ -1,6 +1,6 @@
 /*
-Allison Paul, Samuel Hsiang, Vaughan McDonald
-*/
+   Allison Paul, Samuel Hsiang, Vaughan McDonald
+   */
 
 import java.lang.*;
 import java.io.PrintWriter;
@@ -10,22 +10,47 @@ import java.net.Socket;
 
 public class Bot
 {
-    public static void main(String[] args)
-    {
-        try
-        {
-            Socket skt = new Socket("test-exch-same", 20000);
-            BufferedReader from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
-            PrintWriter to_exchange = new PrintWriter(skt.getOutputStream(), true);
+	// connection to server
+	public static Socket skt;
+	public static BufferedReader from_exchange;
+	public static PrintWriter to_exchange;
 
-            to_exchange.println("HELLO SAME");
-            String reply = from_exchange.readLine().trim();
-            System.err.printf("The exchange replied: %s\n", reply);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace(System.out);
-        }
-    }
+	public static void main(String[] args)
+	{
+		// setting up connection
+		try
+		{
+			skt = new Socket("test-exch-same", 20000);
+			from_exchange = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+			to_exchange = new PrintWriter(skt.getOutputStream(), true);
+
+			to_exchange.println("HELLO SAME");
+			String reply = from_exchange.readLine().trim();
+			System.err.printf("The exchange replied: %s\n", reply);
+
+			while(true) {
+				try {
+					handleIncomingMessages();
+					executeTrades();
+				} catch(Exception e) {
+					e.printStackTrace(System.out);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace(System.out);
+		}
+	}
+
+	public static void handleIncomingMessages() {
+		for(String message = from_exchange.nextLine(); message != null; message = from_exchange.nextLine()) {
+			// do stuff
+		}
+	}
+
+	public static void executeTrades() {
+		// do stuff
+	}
 }
 
