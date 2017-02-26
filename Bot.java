ee555a0d8dc089ec6720c9ebd05c0bdb9a3ecec2
Samuel Hsiang <samuel.c.hsiang@gmail.com>
2017-02-26 11:25:46 -0500

    fix

--- Bot.java
+++ Bot.java
@@ -3,9 +3,7 @@
    */
 
 import java.lang.*;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.io.*;
 import java.net.Socket;
 
 public class Bot
